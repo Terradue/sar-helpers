4ae42726da697f1ba4d38c3225c13b5c08f5a5e5
fabricebrito <fabrice.brito@terradue.com>
2015-01-04 18:34:35 +0100

    working on pom and artifacts

--- src/main/resources/lib/sar-helpers.sh
+++ src/main/resources/lib/sar-helpers.sh
@@ -1,28 +1,124 @@
 #!/bin/bash
 
-get_MIMEtype() {
-  
+# /*!
+#     This is a comment about FunctionName.
+#  */
+__get_MIMEtype() {
+  set -o pipefail 
   local file=$1
   out=`file -bi $file | cut -d ";" -f1`
-  echo a ${PIPESTATUS[0]}
-  echo b ${PIPESTATUS[1]}
-  echo c ${PIPESTATUS[*]} 
-  [[ ${PIPESTATUS[0]} != 0 ]] || [[ ${PIPESTATUS[1]} != 0 ]] && return 1
+  [ $? != 0 ] && return 1
   echo $out
-
 } 
 
 #GetSensingDate
 
-#GetArchiveContent
+__get_archive_content() {
+  local dataset="$1"
+  local mimetype=`__get_MIMEtype $dataset`
+
+  case $mimetype in
+    "application/x-tar")
+      content=`tar tf $dataset`
+      res=$?
+      ;;
+    "application/octet-stream")
+      content=$dataset
+      res=$?
+      ;;
+  esac
+
+  [ $res != 0 ] && return 1
+  echo $content
+}
 
 #GetCEOSSensingDate
 
-get_Envisat_sensing_date() {
+__get_ASAR_sensing_date() {
   local dataset="$1"
-  res=`sed -b -n "10,10p" $dataset | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d`
-  
-  echo $res
+  set -o pipefail
+ 
+  local mimetype=`__get_MIMEtype $dataset`
+
+  case $mimetype in
+    "application/zip")
+      sensingdate=`zcat -f $dataset | sed -b -n "10,10p" | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d` 
+      res=$?
+      ;;
+    "application/octet-stream")
+      sensingdate=`sed -b -n "10,10p" $dataset | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d`
+      res=$?
+      ;;
+  esac
+
+  [ $res != 0 ] && return 1
+  echo $sensingdate
+}
+
+__get_ERSCEOS_sensing_date() {
+  local dataset="$1"
+  set -o pipefail
+
+  local mimetype=`__get_MIMEtype $dataset`
+  case $mimetype in
+    "application/x-tar")
+      lea=`__get_archive_content $dataset | tr " " "\n" | grep --ignore-case lea_01.001`
+      tmpdir=/tmp/.`uuidgen`
+      mkdir -p $tmpdir
+      tar -xOf $dataset $lea > $tmpdir/lea_01.001   
+      sensingdate=`metadata -dssr $tmpdir/lea_01.001 | grep "Zero-Doppler azimuth time center pixel"  | cut -c 40-63 | xargs -I {} date --date="{}" +"%Y%m%d"`
+      res=$?
+      rm -fr $tmpdir
+      ;;
+  esac
+
+  [ $res != 0 ] && return 1
+  echo $sensingdate
+}
+
+__get_ERSCEOS_mission() {
+  local dataset="$1"
+  set -o pipefail
+  local mimetype=`__get_MIMEtype $dataset`
+  case $mimetype in
+    "application/x-tar")
+      lea=`__get_archive_content $dataset | tr " " "\n" | grep --ignore-case lea_01.001`
+      tmpdir=/tmp/.`uuidgen`
+      mkdir -p $tmpdir
+      tar -xOf $dataset $lea > $tmpdir/lea_01.001
+      mission=`metadata -dssr $tmpdir/lea_01.001 | grep "MISSION ID" | sed 's/MISSION ID//' | tr -d "\t "`
+      res=$?
+      rm -fr $tmpdir
+      ;;
+  esac
+
+  [ $res != 0 ] && return 1
+  echo $mission
+}
+
+__get_ERSCEOS_cycle() {
+    local dataset="$1"
+  set -o pipefail
+  local mimetype=`__get_MIMEtype $dataset`
+  case $mimetype in
+    "application/x-tar")
+      lea=`__get_archive_content $dataset | tr " " "\n" | grep --ignore-case lea_01.001`
+      tmpdir=/tmp/.`uuidgen`
+      mkdir -p $tmpdir
+      tar -xOf $dataset $lea > $tmpdir/lea_01.001
+      mission=`metadata -dssr $tmpdir/lea_01.001 | grep "MISSION ID" | sed 's/MISSION ID//' | tr -d "\t "`
+      res=$?
+      if [ $mission == ERS1 ]; then
+        cycle=`__get_ERS1_cycle $tmpdir/lea_01.001`
+      fi
+      rm -fr $tmpdir
+      ;;
+  esac
+
+  [ $res != 0 ] && return 1
+  echo $mission
+
+
 }
 
 #GetCSKSensingDate
--- src/main/tests/sar-helpers_tests.sh
+++ src/main/tests/sar-helpers_tests.sh
@@ -1,27 +1,88 @@
 #!/bin/bash
 
 testDirectoryMIME() {
-  out=`get_MIMEtype $(dirname $0)`
+  out=`__get_MIMEtype $_ROOT`
   assertEquals "Failed on directory MIME type" \
   "application/x-directory" "$out"
 }
 
 
 testEnvisatMIME() {
-  out=`get_MIMEtype $(dirname $0)/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1`
+  out=`__get_MIMEtype $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1`
   assertEquals "Failed to retrieve Envisat MIME type" \
   "application/octet-stream" "$out"
 }
 
-testEnvisatMIMEzip() {
-  out=`get_MIMEtype $(dirname $0)/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.zip`
+testMIMEzip() {
+  out=`__get_MIMEtype $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.zip`
   assertEquals "Failed to retrieve Envisat MIME type" \
   "application/zip" "$out"
 }
 
+testMIMEtargz() {
+  out=`__get_MIMEtype $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tar.gz`
+  assertEquals "Failed to retrieve Envisat MIME type" \
+  "application/x-gzip" "$out"
+}
+
+testMIMEtgz() {
+  out=`__get_MIMEtype $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tgz`
+  assertEquals "Failed to retrieve Envisat MIME type" \
+  "application/x-gzip" "$out"
+}
+
+testMIMEgz() {
+  out=`__get_MIMEtype $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.gz`
+  assertEquals "Failed to retrieve Envisat MIME type" \
+  "application/x-gzip" "$out"
+}
+
+testMIMEtar() {
+  out=`__get_MIMEtype $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tar`
+  assertEquals "Failed to retrieve Envisat tar MIME type" \
+  "application/x-tar" "$out"
+
+  out=`__get_MIMEtype $_ROOT/artifacts/ER01_SAR_RAW_0P_20000217T095726_20000217T095743_44928.CEOS_low_case.tar`
+  assertEquals "Failed to retrieve ERS CEOS low-case tar MIME type" \
+  "application/x-tar" "$out"
+}
+
+testASAsensingdate() {
+  out=`__get_ASAR_sensing_date $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1`
+  assertEquals "Failed to retrieve Envisat ASAR sensing date" \
+  "20120329" "$out"
+}
+
+testASAsensingdatezip() {
+  out=`__get_ASAR_sensing_date $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.zip`
+  assertEquals "Failed to retrieve Envisat ASAR sensing date" \
+  "20120329" "$out"
+}
+
+testERSCEOSlowcase() {
+  out=`__get_archive_content $_ROOT/artifacts/ER01_SAR_RAW_0P_20000217T095726_20000217T095743_44928.CEOS_low_case.tar`
+  assertEquals "Failed to retrieve ERS CEOS tar content" \
+  "pablo/ pablo/TEMP_DN/ pablo/TEMP_DN/TMP_MARINA/ pablo/TEMP_DN/TMP_MARINA/TERRAFIRMA/ pablo/TEMP_DN/TMP_MARINA/TERRAFIRMA/TMP/ pablo/TEMP_DN/TMP_MARINA/TERRAFIRMA/TMP/dat_01.001 pablo/TEMP_DN/TMP_MARINA/TERRAFIRMA/TMP/lea_01.001 pablo/TEMP_DN/TMP_MARINA/TERRAFIRMA/TMP/nul_dat.001 pablo/TEMP_DN/TMP_MARINA/TERRAFIRMA/TMP/vdf_dat.001" "$out"
+  
+}
+
+testERSCEOSlowcasesensingdate() {
+  out=`__get_ERSCEOS_sensing_date $_ROOT/artifacts/ER01_SAR_RAW_0P_20000217T095726_20000217T095743_44928.CEOS_low_case.tar`
+  assertEquals "Failed to ERS CEOS tar low case sensing date" \
+  "20000217" "$out"
+}
+
+testERSCEOSlowcasemission() {
+  out=`__get_ERSCEOS_mission $_ROOT/artifacts/ER01_SAR_RAW_0P_20000217T095726_20000217T095743_44928.CEOS_low_case.tar`
+  assertEquals "Failed to ERS CEOS tar low case mission" \
+  "ERS1" "$out"
+}
+
+
 setUp() {
   # load include to test
-  . `dirname $0`/../resources/lib/sar-helpers.sh
+  export _ROOT=`dirname $0`
+  . $_ROOT/../resources/lib/sar-helpers.sh
 }
 
 # load shunit2
