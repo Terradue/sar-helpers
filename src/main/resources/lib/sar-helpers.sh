6b001feaeb4d67e067ab50435849a2dd55a8bc71
fabricebrito <fabrice.brito@terradue.com>
2015-01-05 23:58:55 +0100

    update error handling in getMIMEtype

--- src/main/resources/lib/sar-helpers.sh
+++ src/main/resources/lib/sar-helpers.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
 
 # /*!
-#     This is a comment about FunctionName.
+#     __get_MIMEtype() is an internal function
+#     Its purpose is to find the mime type to allo invoking functions to handle correctly the input
 #
+#     @param $1 path to input file
 #
-#     @throws foo_exception
+#     @return Echoes MIME type to stdout on success, 1 on failure.
 #
-#     @throws bar_exception
-#
-#     @namespace I/O Kit (this is just a string)
-#
-#     @updated 2003-03-15
+#     @updated 2014-01-05
 #  */
 __get_MIMEtype() {
   set -o pipefail 
   local file=$1
-  out=`file -bi $file | cut -d ";" -f1`
-  [ $? != 0 ] && return 1
+  mime=`file -bi $file`
+  [[ $mime == *"(No such file or directory)"* ]] && return 1
+  out=`echo $mime | cut -d ";" -f1`
   echo $out
 } 
 
 #GetSensingDate
 
 # /*!
-#     This is a comment about FunctionName.
-#
-#     @discussion this is the discussion
-#
-#     @throws foo_exception
-#
-#     @throws bar_exception
-#
-#     @namespace I/O Kit (this is just a string)
+#     The function __get_archive_content() returns the content of an archive provided as argument
 #
 #     @updated 2003-03-15
 #  */
 __get_archive_content() {
   local dataset="$1"
   local mimetype=`__get_MIMEtype $dataset`
-
+ set +x 
   case $mimetype in
     "application/x-tar")
       content=`tar tf $dataset`
@@ -47,6 +38,21 @@ __get_archive_content() {
     "application/octet-stream")
       content=$dataset
       res=$?
+      ;;
+    "application/zip")
+      content=`zipinfo -1 $dataset`
+      res=$?
+      ;;
+    "application/x-gzip")
+      content=`zcat -lv $dataset | sed '2q;d' | awk '{ print $9 }'`
+      res=$?
+      if [[ "$content" =~ .*\.tar.* ]]; then
+        content=`tar tfz $dataset`
+        res=`echo $res + $? | bc`
+      else
+        content=${content#`dirname $content`\/}
+        res=`echo $res + $? | bc`
+      fi
       ;;
   esac
 
@@ -63,13 +69,29 @@ __get_ASAR_sensing_date() {
   local mimetype=`__get_MIMEtype $dataset`
 
   case $mimetype in
+    "application/x-tar")
+      sensingdate=`tar -Oxf $dataset | sed '10q;d' | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d`
+      res=$?
+      ;;
     "application/zip")
       sensingdate=`zcat -f $dataset | sed -b -n "10,10p" | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d` 
       res=$?
       ;;
     "application/octet-stream")
+      # TODO check if it's a ASA_IM__0
       sensingdate=`sed -b -n "10,10p" $dataset | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d`
       res=$?
+      ;;
+    "application/x-gzip")
+      content=`zcat -lv $dataset | sed '2q;d' | awk '{ print $9 }'`
+      res=$?
+      if [[ "$content" =~ .*\.tar.* ]]; then
+        sensingdate=`tar -Oxf $dataset | sed '10q;d' | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d`
+        res=`echo $res + $? | bc`
+      else
+        sensingdate=`zcat $dataset | sed '10q;d' | cut -b 16-26 | xargs -I {} date -d {} +%Y%m%d`
+        res=`echo $res + $? | bc`
+      fi
       ;;
   esac
 
@@ -80,20 +102,27 @@ __get_ASAR_sensing_date() {
 __get_ERSCEOS_sensing_date() {
   local dataset="$1"
   set -o pipefail
-
+  set +x
+  
   local mimetype=`__get_MIMEtype $dataset`
+  local lea=`__get_archive_content $dataset | tr " " "\n" | grep --ignore-case lea_01.001`
+  local tmpdir=/tmp/.`uuidgen`
+  mkdir -p $tmpdir
+
   case $mimetype in
     "application/x-tar")
-      lea=`__get_archive_content $dataset | tr " " "\n" | grep --ignore-case lea_01.001`
-      tmpdir=/tmp/.`uuidgen`
-      mkdir -p $tmpdir
       tar -xOf $dataset $lea > $tmpdir/lea_01.001   
-      sensingdate=`metadata -dssr $tmpdir/lea_01.001 | grep "Zero-Doppler azimuth time center pixel"  | cut -c 40-63 | xargs -I {} date --date="{}" +"%Y%m%d"`
       res=$?
-      rm -fr $tmpdir
+      ;;
+    "application/x-gzip")
+      tar -xzOf $dataset $lea > $tmpdir/lea_01.001
+      res=$?
       ;;
   esac
-
+  
+  sensingdate=`metadata -dssr $tmpdir/lea_01.001 | grep "Zero-Doppler azimuth time center pixel"  | cut -c 40-63 | xargs -I {} date --date="{}" +"%Y%m%d"`
+  res=`echo $res + $? | bc`
+  rm -fr $tmpdir
   [ $res != 0 ] && return 1
   echo $sensingdate
 }
@@ -143,17 +172,25 @@ __get_ERSCEOS_cycle() {
 
 }
 
-#GetCSKSensingDate
+# get_mission()
 
-#GetTSXSensingDate
+# get_sensing_date()
 
-#GetRadarsatSensingDate
+# get_cycle()
 
-#GetSentinel1SensingDate
+# get_track()
 
-#CreateAdoreEnv
+# __get_CSK_sensing_date()
 
-#CreateGMTSAREnv
+# __get_TSX_sensing_date
 
-#CreateROIPACEnv
+# __get_Radarsat_sensing_date
+
+# __get_Sentinel1_sensing_date
+
+# create_env_adore()
+
+# create_env_gmtsar()
+
+# create_env_roipac()
 
