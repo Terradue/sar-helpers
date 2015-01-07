6ed5348842e4111ba88e00b1f367c8d32404e777
fabricebrito <fabrice.brito@terradue.com>
2015-01-07 17:26:30 +0100

    update roi_pac

--- src/main/tests/sar-helpers_tests.sh
+++ src/main/tests/sar-helpers_tests.sh
@@ -368,9 +368,10 @@ testGetSensingdate_ERS1E1() {
 # GMTSAR
 #
 #
+
 testLinkASAR_gmtsar() {
   mkdir -p $_TEST/raw
-  out=`__link_ASAR_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1 $_TEST/raw`
+  out=`__link_N1E1E2_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1 $_TEST/raw`
   res=$? 
   assertEquals "link ASAR GMTSAR failed" \
   "0" "$res"
@@ -378,11 +379,165 @@ testLinkASAR_gmtsar() {
 
 testLinkASAR_gmtsar_tar() {
   mkdir -p $_TEST/raw
-  out=`__link_ASAR_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tar $_TEST/raw`
+  out=`__link_N1E1E2_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tar $_TEST/raw`
   res=$?
-  assertEquals "link ASAR tar GMTSAR failed" \
+  assertEquals "extract ASAR tar GMTSAR failed" \
   "0" "$res"
 }
+
+testLinkASAR_gmtsar_zip() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.zip $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR zip GMTSAR failed" \
+  "0" "$res"
+}
+
+testLinkASAR_gmtsar_gz() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.gz $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR gz GMTSAR failed" \
+  "0" "$res"
+}
+
+testLinkASAR_gmtsar_tgz() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_gmtsar $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tgz $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR tgz GMTSAR failed" \
+  "0" "$res"
+}
+
+testLinkERSCEOS_gmtsar_tgz() {
+  mkdir -p $_TEST/raw
+  out=`__link_ERSCEOS_gmtsar $_ROOT/artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tgz $_TEST/raw`
+  res=$?
+  assertEquals "extract ERS CEOS tgz GMTSAR failed" \
+  "0" "$res"
+}
+
+testLinkERSCEOS_gmtsar_tar() {
+  mkdir -p $_TEST/raw
+  out=`__link_ERSCEOS_gmtsar $_ROOT/artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tar $_TEST/raw`
+  res=$?
+  assertEquals "extract ERS CEOS tar GMTSAR failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testCreate_env_gmtsar_ASAR() {
+  mkdir -p $_TEST
+  master="$_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tgz"
+  slave="$_ROOT/artifacts/ASA_IM__0CNPDE20111031_071059_000000163108_00121_50566_4642.N1"
+  create_env_gmtsar $master $slave $_TEST
+  res=$?
+  assertEquals "create GMTSAR env with ASAR" \
+  "0" "$res"
+}
+
+testCreate_env_gmtsar_ERSCEOS() {
+  mkdir -p $_TEST
+  master="$_ROOT/artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tar.gz"
+  slave="$_ROOT/artifacts/ER02_SAR_RAW_0P_20060807T174703_20060807T174719_ESR_59070.CEOS.tgz"
+  create_env_gmtsar $master $slave $_TEST
+  res=$?
+  assertEquals "create GMTSAR env with ERS CEOS" \
+  "0" "$res"
+  find $_TEST
+}
+
+#
+#
+# ROI_PAC
+#
+#
+testLinkASAR_roipac() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_roipac $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1 $_TEST/raw`
+  res=$?
+  assertEquals "link ASAR ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testLinkASAR_roipac_tar() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_roipac $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tar $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR tar ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testLinkASAR_roipac_zip() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_roipac $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.zip $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR zip ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testLinkASAR_roipac_gz() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_roipac $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.gz $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR gz ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testLinkASAR_roipac_tgz() {
+  mkdir -p $_TEST/raw
+  out=`__link_N1E1E2_roipac $_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tgz $_TEST/raw`
+  res=$?
+  assertEquals "extract ASAR tgz ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testLinkERSCEOS_roipac_tgz() {
+  mkdir -p $_TEST/raw
+  out=`__link_ERSCEOS_roipac $_ROOT/artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tgz $_TEST/raw`
+  res=$?
+  assertEquals "extract ERS CEOS tgz ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testLinkERSCEOS_roipac_tar() {
+  mkdir -p $_TEST/raw
+  out=`__link_ERSCEOS_roipac $_ROOT/artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tar $_TEST/raw`
+  res=$?
+  assertEquals "extract ERS CEOS tar ROI_PAC failed" \
+  "0" "$res"
+  find $_TEST
+}
+
+testCreate_env_roipac_ASAR() {
+  mkdir -p $_TEST
+  master="$_ROOT/artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1.tgz"
+  slave="$_ROOT/artifacts/ASA_IM__0CNPDE20111031_071059_000000163108_00121_50566_4642.N1"
+  create_env_roipac $master $slave $_TEST
+  res=$?
+  assertEquals "create GMTSAR env with ASAR" \
+  "0" "$res"
+  find $_TEST
+}
+
+testCreate_env_roipac_ERSCEOS() {
+  mkdir -p $_TEST
+  master="$_ROOT/artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tar.gz"
+  slave="$_ROOT/artifacts/ER02_SAR_RAW_0P_20060807T174703_20060807T174719_ESR_59070.CEOS.tgz"
+  create_env_roipac $master $slave $_TEST
+  res=$?
+  assertEquals "create GMTSAR env with ERS CEOS" \
+  "0" "$res"
+  find $_TEST
+}
+
+
 
 setUp() {
   # load include to test
@@ -390,6 +545,10 @@ setUp() {
   . $_ROOT/../resources/lib/sar-helpers.sh
   mkdir -p $_ROOT/runtime
   export _TEST=$_ROOT/runtime
+}
+
+tearDown() {
+  [ -d $_TEST ] && rm -fr $_TEST
 }
 
 # load shunit2
