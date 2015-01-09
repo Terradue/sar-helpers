98df734e7c1ba3bf2d05a47714dcdea860a0f142
fabricebrito <fabrice.brito@terradue.com>
2015-01-09 15:54:23 +0100

    documented gmtsar.sh and add gatherdoc

--- src/main/resources/lib/exec.d/gmtsar.sh
+++ src/main/resources/lib/exec.d/gmtsar.sh
@@ -2,6 +2,17 @@
 
 set -o pipefail
 
+# /*!
+# __link_N1E1E2_gmtsar() is an internal function that links (with the extension .baq) the Envisat ASAR (.N1) and ERS-1/2 SAR dataset (.E1 or .E2) in the folder provided as second argument to be used with GMTSAR. 
+#
+# @param $1 path to (A)SAR dataset in .N1 , .E1 or .E2 format 
+#
+# @param $2 target folder (will be created if it doesn't exist)
+#
+# @return 0 on success, 1 on failure.
+#
+# @updated 2014-01-09
+# */
 __link_N1E1E2_gmtsar() {
   # TODD add unit test
   local dataset="$1"
@@ -41,6 +52,17 @@ __link_N1E1E2_gmtsar() {
   return $res
 }
 
+# /*!
+# __link_ERSCEOS_gmtsar() is an internal function that links (with .ldr and .dat extension) the ERS-1/2 SAR dataset in CEOS format in the folder provided as second argument to be used with GMTSAR.
+#
+# @param $1 path to ERS-1/2 SAR dataset in CEOS format
+#
+# @param $2 target folder (will be created if it doesn't exist)
+#
+# @return 0 on success, 1 on failure.
+#
+# @updated 2014-01-09
+# */
 __link_ERSCEOS_gmtsar() {
   local dataset="$1"
   local target="$2"
@@ -67,10 +89,25 @@ __link_ERSCEOS_gmtsar() {
   return 0
 }
 
+# /*!
+# create_env_gmtsar() is a public function that links a master and a slave dataset in the folder provided as third argument to be used with GMTSAR. The function will create a new folder named 'raw'
+#
+# @param $1 path to the master dataset 
+#
+# @param $2 path to the slave dataset 
+#
+# @param $3 target folder (will be created if it doesn't exist) - e.g. runtime
+#
+# @return 0 on success, 1 on failure.
+#
+# @updated 2014-01-09
+# */
 create_env_gmtsar() {
   local master="$1"
   local slave="$2"
   local target="$3"
+
+  # TODO check how many parameters were provided
 
   mission=$( get_mission $master )
 
