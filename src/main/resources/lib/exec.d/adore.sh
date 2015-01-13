6a4ff6a5d60b2241e447ac2c9fef849c2d6ff1d5
fabricebrito <fabrice.brito@terradue.com>
2015-01-13 14:42:42 +0100

    adore and TSX

--- src/main/resources/lib/exec.d/adore.sh
+++ src/main/resources/lib/exec.d/adore.sh
@@ -2,13 +2,16 @@
 
 set -o pipefail
 
-# create_env_adore()
-
+# /*!
+# __link_N1E1E2_adore() is an internal function to link Envisat ASAR or ERS-1/2 in Envisat format (.E1, .E2)
 #
+# @param $1 path to the Envisat ASAR or ERS-1/2 in Envisat format (.E1, .E2) dataset
+# @param $2 target folder for creating the Doris/Adore structure and data 
 #
-# ADORE Doris
+# @return 0 on success, 1 on failure.
 #
-#
+# @updated 2014-01-13
+# */
 __link_N1E1E2_adore() {
   local dataset="$1"
   local target="$2"  
@@ -18,7 +21,19 @@ __link_N1E1E2_adore() {
   return $?
 }
 
+
+# /*!
+# __link_TSX_adore() is an internal function to link Envisat ASAR or ERS-1/2 in Envisat format (.E1, .E2)
+#
+# @param $1 path to the TerraSAR-X dataset
+# @param $2 target folder for creating the Doris/Adore structure and data
+#
+# @return 0 on success, 1 on failure.
+#
+# @updated 2014-01-13
+# */
 __link_TSX_adore() {
+  
   local dataset="$1"
   local target="$2"
   local sensing_date
@@ -28,6 +43,10 @@ __link_TSX_adore() {
 
   local mimetype=`__get_MIMEtype $dataset`
   [ $? != 0 ] && return 1
+
+  local content
+  content=$( __get_archive_content ${dataset} )
+  [ $? != 0 ] && return 1 
 
   local leader=$( echo $content | tr " " "\n" | grep "\.xml$" | sed 's#.*/\(.*\)#\1#g' | grep "^T.*\.xml" )
   [ $? != 0 ] && return 1
@@ -51,52 +70,94 @@ __link_TSX_adore() {
       ;;
   esac
 
+  # TODO check $res
   return 0
 
 }
 
+
+# /*!
+# create_env_adore() is a public function to create the processing environment for Doris/Adore
+#
+# @param $1 path to the master dataset
+# @param $2 path to the slave dataset
+# @param $2 target folder for creating the Doris/Adore structure and data
+#
+# @return 0 on success, 1 on failure.
+#
+# @updated 2014-01-13
+# */
 create_env_adore() {
+set -x
   local master="$1"
   local slave="$2"
   local target="$3"
 
   mission=`get_mission $master`
 
-  mkdir -p $target/datafolder
+  mkdir -p $target/data
   [ $? != 0 ] && return 1
 
+  local settings
+  settings=${target}/settings.set
+
+  local m_sensing_date
+  m_sensing_date=$( get_sensing_date $master )
+  res=$?
+
+  local s_sensing_date
+  s_sensing_date=$( get_sensing_date $slave )
+  res=$?
+
   # TODO add check on mission_slave != mission_master (deal with tandem)
-  for sar in $master $slave
-  do
-    case $mission in
-      "ASAR")
-        __link_N1E1E2_adore $sar $target/datafolder
-        res=$?
-        echo > ${target}/settings << EOF
+  case $mission in
+    "ASAR")
+      __link_N1E1E2_adore ${master} ${target}/data
+      res=$?
+      
+      __link_N1E1E2_adore ${slave} ${target}/data
+      res=$?
+
+      echo > ${settings} << EOF
 m_in_method='ASAR'
-dataFile="ASA*.N1"
-s_in_method="ASAR"
+s_in_method='ASAR'
+m_in_dat="${target}/data/$( basename ${master} )"
+s_in_dat="${target}/data/$( basename ${slave} )"
+master=$( basename ${master} )
+slave=$( basename ${slave} )
 EOF
-        ;;
-      "TSX" | "TDX")
-        __link_TSX_adore $sar $target/datafolder
-        res=$?
-        echo > ${target}/settings << EOF
+      ;;
+    "TSX" | "TDX")
+      # TODO check $res
+      __link_TSX_adore ${master} ${target}/data
+      res=$?
+  
+      __link_TSX_adore ${slave} ${target}/data
+      res=$?
+  
+      cat > ${settings} << EOF
 m_in_method='TSX'
 s_in_method="TSX"
-dataFile="*.cos"
-leaderFile="*.xml"
+m_in_dat="${target}/data/${m_sensing_date}.cos"
+s_in_dat="${target}/data/${s_sensing_date}.cos"
+m_in_lea="${target}/data/${m_sensing_date}.xml"
+s_in_lea="${target}/data/${s_sensing_date}.xml"
+master=${m_sensing_date}
+slave=${s_sensing_date}
 EOF
-        ;;
-      "ERS1-CEOS" | "ERS2-CEOS")
-        __link_ERSCEOS_ro $sar $target/raw
-        res=$?
-        ;;
-      *)
-        return 1
-        ;;
-      esac
-  done
+      ;;
+    "ERS1-CEOS" | "ERS2-CEOS")
+      __link_ERSCEOS_adore $sar $target/raw
+      res=$?
+      ;;
+    *)
+      return 1
+      ;;
+    esac
 
+  [ -e ${settings} ] && sed -i  's/^/settings apply -r -q /' $settings  
+  res=$?
+set +x
   return $?
+
 }
