445d0046bb3a4d2f7b1e2e0516750b98e043b0b7
fabricebrito <fabrice.brito@terradue.com>
2015-01-19 22:20:35 +0100

    add check on args #3

--- src/main/tests/tests.d/roipac-tests.sh
+++ src/main/tests/tests.d/roipac-tests.sh
@@ -103,4 +103,26 @@ testCreate_env_roipac_ERSCEOS() {
   find $_TEST
 }
 
+export _ROOT=$( pwd )
+source ${_ROOT}/../resources/lib/sar-helpers.sh
+
+testCreate_env_roipac_different_missions() {
+  master=$_ROOT/../artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1
+  slave=$_ROOT/../artifacts/ER02_SAR_RAW_0P_20100118T174929_20100118T174945_ESR_77106.CEOS.tar
+  root=$( create_env_roipac ${master} ${slave} ${_TEST} )
+  res=$?
+  assertEquals "Different missions must not be accepted" \
+  "1" "$res"
+}
+
+testCreate_env_roipac_different_tracks() {
+  # we know we passed L1 and L0, we saved some storage space by doing this
+  master=$_ROOT/../artifacts/ASA_IM__0CNPDE20120329_071134_000000163113_00121_52721_6279.N1
+  slave=$_ROOT/../artifacts/ASA_IM__0CNPDE20111031_071059_000000163108_00121_50566_4642.N1
+  root=$( create_env_roipac ${master} ${slave} ${_TEST} )
+  res=$?
+  assertEquals "Different missions must not be accepted" \
+  "1" "$res"
+}
+
 . ./common-tests.inc
